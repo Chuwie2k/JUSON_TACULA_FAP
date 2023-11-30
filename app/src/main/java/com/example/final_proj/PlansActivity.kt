f20b35bc2ebe5ede7877fb9202931e7178e488a8
TaculaIT <karl.tacula.cics@ust.edu.ph>
2023-11-30 08:54:57 +0800

    plan data listener

--- app/src/main/java/com/example/final_proj/PlansActivity.kt
+++ app/src/main/java/com/example/final_proj/PlansActivity.kt
@@ -2,68 +2,78 @@ package com.example.final_proj
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
-
 import androidx.appcompat.app.AppCompatActivity
-
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.*
 
 class PlansActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?,) {
+    private lateinit var plansTableLayout: TableLayout
+    private lateinit var goToMainMenuButton: ImageButton
+    private lateinit var databaseReference: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plan)
 
-        val plansTableLayout: TableLayout = findViewById(R.id.plansTableLayout)
-        val goToMainMenuButton : ImageButton = findViewById(R.id.goToMainMenuButton)
+        plansTableLayout = findViewById(R.id.plansTableLayout)
+        goToMainMenuButton = findViewById(R.id.goToMainMenuButton)
+
+        FirebaseApp.initializeApp(this)
+        databaseReference = FirebaseDatabase.getInstance().reference.child("uniqueID").child("plans")
+
+        // Read data from Firebase
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Clear existing rows
+                plansTableLayout.removeAllViews()
+
+                for (planSnapshot in dataSnapshot.children) {
+                    // Get the plan data
+                    val planData = planSnapshot.getValue(object : GenericTypeIndicator<Map<String, Plan>>() {})
+                    if (planData != null) {
+                        // Iterate through the map entries (key-value pairs)
+                        for ((key, plan) in planData) {
+                            // Create a row for each plan
+                            val row = TableRow(this@PlansActivity)
+
+                            val nameTextView = TextView(this@PlansActivity)
+                            nameTextView.text = plan.name
+                            row.addView(nameTextView)
+
+                            val breakfastTextView = TextView(this@PlansActivity)
+                            breakfastTextView.text = plan.breakfast
+                            row.addView(breakfastTextView)
+
+                            val lunchTextView = TextView(this@PlansActivity)
+                            lunchTextView.text = plan.lunch
+                            row.addView(lunchTextView)
+
+                            val dinnerTextView = TextView(this@PlansActivity)
+                            dinnerTextView.text = plan.dinner
+                            row.addView(dinnerTextView)
+
+                            plansTableLayout.addView(row)
+                        }
+                    }
+                }
+            }
 
 
-        val sampleData = listOf(
-            PlanData("John", "Cereal", "Salad", "Pizza"),
-            PlanData("Alice", "Pancakes", "Sandwich", "Grilled Chicken"),
-            // Add more data as needed
-        )
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("Firebase", "Error reading data: ${databaseError.message}")
+            }
+        })
 
-        // Iterate through the data and create rows dynamically
-        for (plan in sampleData) {
-            val row = TableRow(this)
-
-            val nameTextView = TextView(this)
-            nameTextView.text = plan.name
-            row.addView(nameTextView)
-
-            val breakfastTextView = TextView(this)
-            breakfastTextView.text = plan.breakfast
-            row.addView(breakfastTextView)
-
-            val lunchTextView = TextView(this)
-            lunchTextView.text = plan.lunch
-            row.addView(lunchTextView)
-
-            val dinnerTextView = TextView(this)
-            dinnerTextView.text = plan.dinner
-            row.addView(dinnerTextView)
-
-            plansTableLayout.addView(row)
-        }
-        goToMainMenuButton.setOnClickListener{
-            // Create an Intent to start the MainActivity
-            val intent = Intent(this, CreateActivity::class.java)
-            // Start the MainActivity
+        goToMainMenuButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-            // Finish the current activity
             finish()
         }
     }
-
-
-    // Data class to represent a plan
-    data class PlanData(
-        val name: String,
-        val breakfast: String,
-        val lunch: String,
-        val dinner: String
-    )
-    }
+}
