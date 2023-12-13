5f89d3541fd29f1b8e4fbbeada742436aeee8072
TaculaIT <karl.tacula.cics@ust.edu.ph>
2023-12-13 17:08:52 +0800

    deletebuttoon not working

--- app/src/main/java/com/example/final_proj/PlansActivity.kt
+++ app/src/main/java/com/example/final_proj/PlansActivity.kt
@@ -11,8 +11,11 @@ import com.google.firebase.database.*
 class PlansActivity : AppCompatActivity() {
 
     private lateinit var plansListView: ListView
+    private lateinit var deleteButton: Button
     private lateinit var backButton: Button
     private lateinit var databaseReference: DatabaseReference
+    private lateinit var adapter: ArrayAdapter<String>
+    private var firstPlanId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,53 +25,82 @@ class PlansActivity : AppCompatActivity() {
 
         // Initialize views
         plansListView = findViewById(R.id.plansListView)
+        deleteButton = findViewById(R.id.deleteButton)
         backButton = findViewById(R.id.backButton)
 
-        // Set up a list adapter to display plans
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+        // Initialize the adapter
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1)
         plansListView.adapter = adapter
 
-        // Read plans from the database
-        databaseReference.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // Clear the adapter before adding new plans
-                adapter.clear()
-
-                // Loop through the plans under the 'plans' node
-                for (userSnapshot in dataSnapshot.children) {
-                    for (planSnapshot in userSnapshot.children) {
-                        // Log the raw snapshot for better understanding
-                        Log.d("PlansActivity", "Raw Snapshot: $planSnapshot")
-
-                        // Get the Plan object and add it to the adapter
-                        val name = planSnapshot.child("name").getValue(String::class.java)
-                        val breakfast = planSnapshot.child("breakfast").getValue(String::class.java)
-                        val lunch = planSnapshot.child("lunch").getValue(String::class.java)
-                        val dinner = planSnapshot.child("dinner").getValue(String::class.java)
-
-                        // Log the retrieved values
-                        Log.d("PlansActivity", "Name: $name, Breakfast: $breakfast, Lunch: $lunch, Dinner: $dinner")
-
-                        // Check if all required fields are present
-                        if (name != null && breakfast != null && lunch != null && dinner != null) {
-                            // Append the unique ID and plan details to each plan for identification
-                            adapter.add("ID: ${planSnapshot.key}\nName: $name\n" +
-                                    "Breakfast: $breakfast\nLunch: $lunch\nDinner: $dinner\n")
-                        }
-                    }
-                }
+        // Set click listener for the delete button
+        deleteButton.setOnClickListener {
+            // Delete the first plan entered
+            if (firstPlanId != null) {
+                deletePlan(firstPlanId!!)
             }
-
-
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Handle errors
-            }
-        })
+        }
 
         // Set click listener for the back button
         backButton.setOnClickListener {
             finish()
         }
+
+        // Read plans from the database
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                adapter.clear()
+
+                for (userSnapshot in dataSnapshot.children) {
+                    for (planSnapshot in userSnapshot.children) {
+                        val name = planSnapshot.child("name").getValue(String::class.java)
+                        val breakfast = planSnapshot.child("breakfast").getValue(String::class.java)
+                        val lunch = planSnapshot.child("lunch").getValue(String::class.java)
+                        val dinner = planSnapshot.child("dinner").getValue(String::class.java)
+
+                        if (name != null && breakfast != null && lunch != null && dinner != null) {
+                            val planDetails = "ID: ${planSnapshot.key}\nName: $name\n" +
+                                    "Breakfast: $breakfast\nLunch: $lunch\nDinner: $dinner\n"
+                            adapter.add(planDetails)
+
+                            // Save the ID of the first plan entered
+                            if (firstPlanId == null) {
+                                firstPlanId = planSnapshot.key
+                            }
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle errors
+            }
+        })
     }
+
+    private fun deletePlan(planId: String) {
+        val planReference = databaseReference.child("plans").child(planId)
+
+        // Remove the plan from the database
+        planReference.removeValue()
+            .addOnSuccessListener {
+                // Plan deleted successfully
+                Log.d("PlansActivity", "Plan deleted successfully with ID: $planId")
+
+                // Update the UI after deletion
+                adapter.notifyDataSetChanged()
+
+                // Update firstPlanId if the deleted plan was the first plan
+                if (firstPlanId == planId) {
+                    firstPlanId = null
+                }
+            }
+            .addOnFailureListener { e ->
+                // Handle failure, show an error message or log
+                Log.e("PlansActivity", "Error deleting plan with ID: $planId", e)
+            }
+    }
+
 }
+
+
+
